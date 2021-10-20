5b33a6778a80408a850c806b127693863ad62da4
tounani Abderrahmane <tounaniabderrahmane99@gmail.com>
2021-10-20 23:31:43 +0100

    define pile.h with function

--- pile.h
+++ pile.h
@@ -1,1 +1,25 @@
+#ifndef PILE_H_INCLUDED
+#define PILE_H_INCLUDED
 
+typedef struct {
+	int sommet;
+	int taille;
+	int pile[40];
+}PILE;
+
+int estvide(PILE P){
+	return P.sommet==0;
+}
+
+int estpleine(PILE P){
+	return P.sommet==P.taille;
+}
+
+void empiler(PILE *P,int v){
+	P->pile[P->sommet++]=v;
+}
+int depiler(PILE *P){
+	return P->pile[--P->sommet];
+}
+
+#endif
